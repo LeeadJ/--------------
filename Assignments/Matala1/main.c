094053dfc552e05d958c9e64b1e61c75032ef7ee
LeeadJ <leead123@gmail.com>
2021-11-02 13:03:30 +0200

    Fixed main according to the assignment

--- Assignments/Matala1/main.c
+++ Assignments/Matala1/main.c
@@ -1,31 +1,34 @@
 #include<stdio.h>
 #include"NumClass.h"
 
-void main(){
-    printf("Please enter two numers for the range: ");
-    scanf("%d %d", &a, &b)
-    printf("isPrime: ");
-    for(a; a<=b; a++){
-        if(isPrime(a)==1){
-            printf("%d, ", a);
-        }
-    }
-    printf("isStrong: ");
-    for(a; a<=b; a++){
-        if(isStrong(a)==1){
-            printf("%d, ", a);
-        }
-    }
-    printf("isPalindrome: ");
-    for(a; a<=b; a++){
-        if(isPalindrome(a)==1){
-            printf("%d, ", a);
-        }
-    }
-    printf("isArmstrong: ");
-    for(a; a<=b; a++){
-        if(isArmstrong(a)==1){
-            printf("%d, ", a);
-        }
-    }
+void main(void){
+    int a, b, i;
+   printf(" Hello, how are you? Please enter the two numbers: \n");
+   scanf("%d %d", &a,&b);
+  
+   printf("\n isPrime numbers: ");
+   for(i = a; i<=b; i++){
+        if(isPrime(i) == 1){
+            printf("%d, ", i);         
+     } 
+   } 
+   printf("\n isStrong numbers: ");
+     for(i = a; i<=b; i++){
+        if(isStrong(i) == 1){
+            printf("%d, ", i);          
+     } 
+   } 
+   printf("\n isPalindrome numbers: ");
+     for(i = a; i<=b; i++){
+        if(isPalindrome(i) == 1){
+            printf("%d, ", i);          
+     } 
+   } 
+   printf("\n isArmostrong numbers: ");
+     for(i= a; i<=b; i++){
+        if(isArmstrong(i) == 1){
+            printf("%d, ", i);          
+     } 
+   } 
+   printf("\n Done! \n");
 }
